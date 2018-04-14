e3af1a93f8b9d9874b8ad021bda72e1d6dce8d23
Luo jie <jie.luo@rea-group.com>
2018-04-14 18:19:21 +0800

    Add test to ensure project environment

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -1,7 +1,10 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class DollarTest {
     @Test
-    public void name() {
+    public void test() {
+        assertEquals(1, 1);
     }
 }
